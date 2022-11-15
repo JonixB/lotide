ce187de076f21a5d21e34042317ff2300e5fe12f
Nikko Bansil <bjohnnikko@gmail.com>
2022-11-15 15:22:40 -0800

    a function that checks if two arrays are identical

---
+++ eqArrays.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(0x2705)} Assertion Passed: ${actual} === ${expected} ${String.fromCodePoint(0x2705)}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${actual} !== ${expected} ${String.fromCodePoint(0x274C)}`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  for (let x = 0; x < arrayOne.length; x++) {
+    if (arrayOne[x] !== arrayTwo[x]) {
+      return false;
+    }
+  }
+  return true;
+};
