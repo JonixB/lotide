a9203c377031b06d26d737a21e3d4d018cc63f53
Nikko Bansil <bjohnnikko@gmail.com>
2022-11-14 17:05:42 -0800

    a function similar to console.assert but with emojis. it compares two values and logs depending if they are equal or not

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x2705) +' Assertion Passed: ' + actual + ' === ' + expected + ' ' + String.fromCodePoint(0x2705));
+  } else {
+    console.log(String.fromCodePoint(0x274C) +' Assertion Failed: ' + actual + ' !== ' + expected + ' ' + String.fromCodePoint(0x274C));
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
