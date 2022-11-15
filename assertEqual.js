db9d1e9cd148d0abe90ec7485b54c4d74a2bb189
Nikko Bansil <bjohnnikko@gmail.com>
2022-11-14 17:13:50 -0800

    made a change to use template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(String.fromCodePoint(0x2705) +' Assertion Passed: ' + actual + ' === ' + expected + ' ' + String.fromCodePoint(0x2705));
+    console.log(`${String.fromCodePoint(0x2705)} Assertion Passed: ${actual} === ${expected} ${String.fromCodePoint(0x2705)}`);
   } else {
-    console.log(String.fromCodePoint(0x274C) +' Assertion Failed: ' + actual + ' !== ' + expected + ' ' + String.fromCodePoint(0x274C));
+    console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${actual} !== ${expected} ${String.fromCodePoint(0x274C)}`);
   }
 };
 
