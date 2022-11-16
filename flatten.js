de279f17186ea9c113c14d0ba6b9d1403f3d87fd
Nikko Bansil <bjohnnikko@gmail.com>
2022-11-15 17:54:37 -0800

    a function that flattens a nested array into a 1-layer, flat array

---
+++ flatten.js
@@ -0,0 +1,13 @@
+const flatten = function(arrToFlatten) {
+  let flattenedArray = [];
+  for (let x = 0; x < arrToFlatten.length; x++) {
+    if (Array.isArray(arrToFlatten[x])) {
+      for (let y = 0; y < arrToFlatten[x].length; y++) {
+        flattenedArray.push(arrToFlatten[x][y]);
+      }
+    } else {
+      flattenedArray.push(arrToFlatten[x]);
+    }
+  }
+  return flattenedArray;
+};
