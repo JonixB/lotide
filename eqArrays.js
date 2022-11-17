341e327aaa97fb9ea25ea68732f33f1b3b540a61
Nikko Bansil <bjohnnikko@gmail.com>
2022-11-16 17:38:10 -0800

    added another condition to check the length of each array being tested

--- eqArrays.js
+++ eqArrays.js
@@ -7,6 +7,9 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
   for (let x = 0; x < arrayOne.length; x++) {
     if (arrayOne[x] !== arrayTwo[x]) {
       return false;
