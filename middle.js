1c6fecab4c880621272f250cdf6df4035ffa6dd6
Nikko Bansil <bjohnnikko@gmail.com>
2022-11-21 14:16:15 -0800

    exported the function

--- middle.js
+++ middle.js
@@ -1,20 +1,3 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  for (let x = 0; x < arrayOne.length; x++) {
-    if (arrayOne[x] !== arrayTwo[x]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArrayEqual = function(arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne,arrayTwo)) {
-    console.log(`${String.fromCodePoint(0x2705)} Assertion Passed: ${arrayOne} === ${arrayTwo} ${String.fromCodePoint(0x2705)}`);
-  } else {
-    console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${arrayOne} !== ${arrayTwo} ${String.fromCodePoint(0x274C)}`);
-  }
-};
-
 const middle = function(array) {
   let middleElement = [];
   if (array.length < 3) {
@@ -29,7 +12,4 @@ const middle = function(array) {
   return middleElement;
 };
 
-console.log(assertArrayEqual(middle([]),[]));
-console.log(assertArrayEqual(middle([1, 2, 3, 4, 5]),[3]));
-console.log(assertArrayEqual(middle([1, 2, 3, 4]),[2, 3]));
-console.log(assertArrayEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]));
+module.exports = middle;
